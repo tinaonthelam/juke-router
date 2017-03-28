fe5185ff7151dd6c0b342161fae6b7da24dc24fb
tina <tina.lam516@gmail.com>
2017-03-28 18:01:02 -0400

    routes redirected

--- browser/react/containers/AppContainer.js
+++ browser/react/containers/AppContainer.js
@@ -109,19 +109,20 @@ export default class AppContainer extends Component {
           <Sidebar deselectAlbum={this.deselectAlbum} />
         </div>
         <div className="col-xs-10">
-        {
-          this.state.selectedAlbum.id ?
-          <Album
-            album={this.state.selectedAlbum}
-            currentSong={this.state.currentSong}
-            isPlaying={this.state.isPlaying}
-            toggleOne={this.toggleOne}
-          /> :
-          <Albums
-            albums={this.state.albums}
-            selectAlbum={this.selectAlbum}
-          />
-        }
+          {
+            this.props.children ?
+              React.cloneElement(this.props.children, {
+
+                album: this.state.selectedAlbum,
+                currentSong: this.state.currentSong,
+                isPlaying: this.state.isPlaying,
+                toggle: this.toggleOne,
+
+                albums: this.state.albums,
+                selectAlbum: this.selectAlbum
+              })
+              : null
+          }
         </div>
         <Player
           currentSong={this.state.currentSong}
--- browser/react/index.js
+++ browser/react/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppContainer from './containers/AppContainer';
+import { Router, Route, hashHistory, IndexRedirect } from 'react-router';
+
+import Albums from './components/Albums.js';
+import Album from './components/Album';
+import Sidebar from './components/Sidebar';
+import Player from './components/Player';
 
 ReactDOM.render(
-  <AppContainer />,
+  <div>
+    <Router history={hashHistory}>
+      <Route exact path="/" component={AppContainer}>
+        <IndexRedirect to="/albums/" />
+        <Route path="/albums" component={Albums} />
+      </Route>
+    </Router>
+  </div>,
   document.getElementById('app')
 );
